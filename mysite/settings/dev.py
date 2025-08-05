957da3a442a84b41e3ad148bfacd498de001f887
Alynne Ferreira <alynneferreiras@gmail.com>
2025-08-05 02:37:16 -0300

    add base

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-6cw7vm+$()2s#vp1%pf30u4gdexo8itr!qwrx5(oir^rb+5ns3"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
